2080b0a3e2a5f7b0dc679cf6a10e36f97afcc2fb
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-07-08 17:50:37 +0200

    delete images

--- img/proc_files/rta.js
+++
@@ -1,5 +0,0 @@
-crtg_content = '';
-function writeCtoCookie(){
-document.cookie='cto_pb=' + escape(crtg_content)+ '; path=/; expires=Fri, 26 Jul 2013 15:38:31 GMT;' + 'domain=.photobucket.com; ';
-}
-writeCtoCookie();
